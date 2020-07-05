82fed5f402d029059802b51fed0af17fd69d7818
Rahul140799 <rahul.venkatesh730@gmail.com>
2020-07-06 01:29:57 +0530

    July challenge Day 5

---
+++ LeetCode/LeetCode July Challenge/hammingDistance.py
@@ -0,0 +1,7 @@
+def hammingDistance(self, x: int, y: int) -> int:
+        result = 0
+        while(x>0 or y>0):
+            result += (x%2) ^ (y%2)
+            x >>= 1
+            y >>= 1
+        return result
